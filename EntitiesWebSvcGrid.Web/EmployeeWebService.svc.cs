6b50c193e0dbbdcec028ccf021d6ec50bff97fd0
davidemoraschi <davidem@eurostrategy.net>
2016-08-23 14:55:28 +0200

    moved to GitHub

---
+++ EntitiesWebSvcGrid.Web/EmployeeWebService.svc.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Activation;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EntitiesWebSvcGrid.Web
+{
+    [ServiceContract(Namespace = "")]
+    [AspNetCompatibilityRequirements(RequirementsMode = AspNetCompatibilityRequirementsMode.Allowed)]
+    public class EmployeeWebService
+    {
+        [OperationContract]
+        public List<Employee> GetEmployees() 
+        { 
+            AdventureWorks_DataEntities ds = new AdventureWorks.DataEntities(); 
+            return ds.Employee.Where(emp => emp.SalariedFlag == true).ToList(); }
+        // Add more operations here and mark them with [OperationContract]
+    }
+}
